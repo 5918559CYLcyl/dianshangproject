168f8c2165a6208d81844307889252c9d17de3aa
Administrator <1250702640@qq.com>
2023-05-23 20:40:45 +0800

    二分类-整体认识和路由配置

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,26 +1,17 @@
 <script setup>
 import HomeCategory from './components/HomeCategory.vue'
-import HomeHot from './components/HomeHot.vue'
+import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
-import HomeBanner from './components/Homebanner.vue'
+import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
-import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
-    <div class="container">
-        <HomeCategory/>
-        <HomeBanner/>
-    </div>
-    <HomeNew/>
-    <HomeHot/>
-    <HomeProduct/>
-    <HomePanel title="新鲜好物" />
-    <HomePanel title="人气推荐" />
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
 </template>
-
-<style scoped>
-
-
-
-</style>
