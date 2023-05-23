2f093f638ffffbae572dc099f2c5cc66bf75e869
Administrator <1250702640@qq.com>
2023-05-23 14:59:33 +0800

    一级分类-面包屑

--- src/router/index.js
+++ src/router/index.js
@@ -9,24 +9,30 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-    path:'/',
-    component:Layout,
-    children:[
-      {
-        path:'',
-        component:Home
-      },
-      {
-        path:'category/:id',
-        component:Category
-      }
-    ]
+      path: '/',
+      name: 'layout',
+      component: Layout,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: Home
+        },
+        {
+          path: 'category/:id',
+          name: 'category',
+          component: Category
+        }
+      ]
     },
     {
-      path:'/login',
-      component:Login
-    }
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+
   ]
 })
 
 export default router
+
