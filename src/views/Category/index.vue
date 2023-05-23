46143bd29911d83c22909959d335ab7128932d26
Administrator <1250702640@qq.com>
2023-05-23 16:09:57 +0800

    一级结束

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,29 +1,16 @@
 <script setup>
-import {getCategoryAPI} from '@/apis/category'
-import {ref,onMounted} from 'vue'
-import {useRoute} from 'vue-router'
-import GoodsItem from '../Home/components/GoodsItem.vue'
-import {onBeforeRouteUpdate} from 'vue-router'
-const categoryData=ref([])
-const route=useRoute()
-const getCategory=async(id=route.params.id)=>{
-    const res = await getCategoryAPI(id)
-    categoryData.value=res.result
-}
-onMounted(()=>getCategory())
-onBeforeRouteUpdate((to)=>{
-    getCategory(to.params.id)
-})
-const bannerList=ref([])
-const getBanner=async()=>{
-    const res=await getBannerAPI({
-        distributionSite:'2'
-    })
-    console.log(res);
-    bannerList.value=res.result
 
-}
-onMounted(()=>getBanner())
+
+
+import GoodsItem from '../Home/components/GoodsItem.vue'
+
+import {useBanner} from './composables/useBanner'
+import {useCategory} from './composables/useCategory'
+
+const {bannerList} =useBanner()
+const {categoryData}=useCategory()
+
+
 
 </script>
 
