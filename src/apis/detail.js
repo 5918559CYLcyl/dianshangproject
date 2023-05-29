20a95541904a0747b293efdefd091a6f94c700b6
Administrator <1250702640@qq.com>
2023-05-29 11:11:26 +0800

    基础数据渲染、基础组件封装

--- src/apis/detail.js
+++ src/apis/detail.js
@@ -8,3 +8,13 @@ export const getDetail=(id)=>{
         }
     })
 }
+export const getHotGoodsAPI=({id,type,limit=3})=>{
+    return request({
+        url:'/goods/hot',
+        params:{
+            id,
+            type,
+            limit
+        }
+    })
+}
