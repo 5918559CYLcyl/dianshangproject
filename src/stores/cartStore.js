42e12d5de285745aa36fdcd00fa03a6ec22b0a56
Administrator <1250702640@qq.com>
2023-06-06 14:06:05 +0800

    本地购物车-购物车列表计算

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,5 +1,6 @@
+import { menuItemEmits } from 'element-plus'
 import {defineStore} from 'pinia'
-import {ref} from 'vue'
+import {computed, ref} from 'vue'
 export const useCartStore=defineStore('cart',()=>{
     const cartList=ref([])
     const addCart=(goods)=>{
@@ -14,10 +15,28 @@ export const useCartStore=defineStore('cart',()=>{
         const idx=cartList.value.findIndex((item)=>skuId===item)
         cartList.value.splice(idx,1)
     }
+    const singleCheck=(skuId,selected)=>{
+        const item=cartList.value.find((item)=>item.skuId===skuId)
+        item.selected=selected
+    }
+    const allCheck=(selected)=>{
+        cartList.value.forEach(item=>item.selected=selected)
+    }
+    const allCount=computed(()=>cartList.value.reduce((a,c)=>a+c.count,0))
+    const allPrice=computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
+
+    const selectedCount=computed(()=>cartList.value.filter(item=>item.selected).reduce((a,c)=>a+c.count,0))
+    const selectedPrice=computed(()=>cartList.value.filter(item=>item.selected).reduce((a,c)=>a+c.count*c.price,0))
+    const isAll=computed(()=>cartList.value.every((item)=>item.selected))
     return{
         cartList,
+        allCount,
+        allPrice,
+        selectedCount,
+        selectedPrice,
         addCart,
-        delCart
+        delCart,
+        isAll
     }
 },{
     persist:true,
